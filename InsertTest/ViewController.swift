479a9413ed4acc2328e9cf7a079ddf820004ee08
Dionte <Dionte_sb@yahoo.com>
2020-02-04 13:15:54 -0800

    add text when clicking on the add button

--- InsertTest/ViewController.swift
+++ InsertTest/ViewController.swift
@@ -12,15 +12,58 @@ import CoreData
 class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var txtField: UITextField!
     
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     var stringArr = [Item]()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        loadItems()
+        
     }
     
+    
+    @IBAction func onClickAddButton(_ sender: UIButton) {
+        
+        let newItem = Item(context: self.context)
+        newItem.name = txtField.text!
+        stringArr.insert(newItem, at: 0)
+        tableView.beginUpdates()
+        tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
+        saveItems()
+        tableView.endUpdates()
+        
+        txtField.text = ""
+        
+    }
+    
+    @IBAction func onClickDeleteButton(_ sender: UIButton) {
+    }
+    
+    
+    func saveItems() {
+        
+        do {
+            try context.save()
+        } catch {
+            print("Error saving items, \(error)")
+        }
+        
+    }
+    
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+        
+        do {
+            stringArr = try context.fetch(request)
+        } catch {
+            print("Error fetching request, \(error)")
+        }
+        
+    }
     
     
 
