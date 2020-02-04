60e6b21f77935c1b8cc56dfb5f03b71cc5ceffab
Dionte <Dionte_sb@yahoo.com>
2020-02-04 13:05:01 -0800

    Implmented the tableView datasource and delegate methods. And setup the core data database

--- InsertTest/ViewController.swift
+++ InsertTest/ViewController.swift
@@ -7,16 +7,46 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    var stringArr = [Item]()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
+    
+    
+    
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stringArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
+        
+        let item = stringArr[indexPath.row]
+        
+        cell.nameLabel.text = item.name
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
+    
+    
+}
